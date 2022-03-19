6562e03c7d7dbef1afcbdb6bd1f9909af58709bf
iqbalaulia <miqbalar12@gmail.com>
2022-03-19 11:46:23 +0700

    [update] pembaharuan tutorial list

--- list.dart
+++ list.dart
@@ -3,8 +3,34 @@ void main(){
   List<dynamic> mahasiswaDynamic = ['iqbal', true,20];
 
   // tipe data yang dapat menyimpan list yang terdapat string
-  List<String> mahasiswaString = ['iqbal', 'aulia','rafi'];
+  List<String> mahasiswaString = ['Iqbal', 'Aulia','Rafi'];
 
-  //  menambahkan list dengan sebuah value
+  //  mengembalikan nilai list pada index tertentu
+  print(mahasiswaString[1]);
+  print(mahasiswaString.elementAt(2));
+
+  // mengembalikan panjang list
+  print(mahasiswaString.length);
+
+  // menambahkan list dengan sebuah nilai
+  mahasiswaString.add('Muhammad');
   print(mahasiswaString);
+
+  //  menambahkan list dengan list
+  List<String> mahasiswa2 = ['Angel','Lina'];
+  mahasiswaString.addAll(mahasiswa2);
+  print(mahasiswaString);
+
+  //  mengurutkan list
+  mahasiswaString.sort();
+  print(mahasiswaString);
+
+  // membalikkan list
+  var mahasiswaBaru = mahasiswaString.reversed.toList();
+  print(mahasiswaBaru);
+
+  // menghapus list
+  mahasiswaString.clear();
+  print(mahasiswaString);
+
 }
