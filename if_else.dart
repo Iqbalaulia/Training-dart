f22ac7471c95afbf4b943f818bfd6d3c8f40d399
iqbalaulia <iqbal.developer16@gmail.com>
2022-03-22 21:05:10 +0700

    switch case

--- if_else.dart
+++ if_else.dart
@@ -10,4 +10,21 @@ void main(){
   // akan dieksekusi jika bernilai false
   // 
   // }
+
+
+  int angka = 6;
+
+  if(angka %2 == 0){
+    print('genap');
+  }else{
+    print('ganjil');
+  }
+
+  String nama = 'iqbal';
+
+  if(nama == 'iqbal'){
+    print('benar iqbal!');
+  }else{
+    print('bukan !!');
+  }
 }
---
+++ switch_case.dart
@@ -0,0 +1,20 @@
+void main(){
+  var nilai = 'F';
+  switch (nilai) {
+    case 'A':
+      print('Sangat baik');
+      break;
+    case 'B':
+      print('Lumayan baik');
+      break;
+    case 'C':
+      print('Baik');
+      break;
+    case 'D':
+      print('Kurang baik');
+      break;
+    
+    default:
+      print('Tidak valid');
+  }
+}
