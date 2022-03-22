08ae532e71781ec87e47101f90e6eff222102bfe
iqbalaulia <iqbal.developer16@gmail.com>
2022-03-22 20:50:47 +0700

    if_else

---
+++ if_else.dart
@@ -0,0 +1,13 @@
+void main(){
+  // decision making
+
+  // if(boolean expression){
+  // 
+  // akan dieksekusi jika bernilai true
+  //
+  // } else{
+  // 
+  // akan dieksekusi jika bernilai false
+  // 
+  // }
+}
