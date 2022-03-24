298b7ce98e6d5e6896cfd70674128b765eb9cb95
iqbalaulia <iqbal.developer16@gmail.com>
2022-03-24 07:58:15 +0700

    challange 4

---
+++ challange4.dart
@@ -0,0 +1,25 @@
+void main(){
+  print('Jawaban Soal 1');
+
+  var n = 2;
+  for (int i = 0; i < n; i++) {
+    var bintang = '';
+    for (int j = 0; j <= i; j++) {
+      bintang = bintang + '*';
+    }
+
+    print(bintang);
+    
+  }
+
+  print('Jawaban Soal 2');
+  var x = 2;
+  for (int k = 0; k < x; k++) {
+    var bintang_bintang = '';
+    for (int l = x; l >= k; l--) {
+      bintang_bintang = bintang_bintang + '*';
+    }
+    print(bintang_bintang);
+    
+  }
+}
